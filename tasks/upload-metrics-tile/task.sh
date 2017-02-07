11b75465e714fa9531e6846e96465021af8eb2cf
Rahul Jain <rjain@pivotal.io>
2017-02-07 15:22:42 -0600

    Updated the scripts

--- tasks/upload-metrics-tile/task.sh
+++ tasks/upload-metrics-tile/task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod +x tool-om/om-linux
+chmod +x om-cli/om-linux
 
 FILE_PATH=`find ./pivnet-metrics-product -name *.pivotal`
 
-./tool-om/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH
+./om-cli/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH
