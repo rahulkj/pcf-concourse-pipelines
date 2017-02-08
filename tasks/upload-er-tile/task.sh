338d965ed95e6ad556584f310eead393fbbd4136
Rahul Jain <rjain@pivotal.io>
2017-02-08 09:53:17 -0600

    Adding stemcell download and upload logic for each tile

--- tasks/upload-er-tile/task.sh
+++ tasks/upload-er-tile/task.sh
@@ -3,4 +3,13 @@ chmod + om-cli/om-linux
 
 FILE_PATH=`find ./pivnet-er-product -name *.pivotal`
 
+STEMCELL_VERSION=`cat ./pivnet-er-product/metadata.json | jq '.Dependencies[32].Release.Version'`
+
+echo "Downloading stemcell"
+pivnet-cli download-product-files -p stemcells -r $STEMCELL_VERSION -g "*vsphere*"
+
+SC_FILE_PATH=`find ./ -name *.tgz`
+
 ./om-cli/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH
+
+./om-cli/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-stemcell -p $SC_FILE_PATH
--- tasks/upload-metrics-tile/task.sh
+++ tasks/upload-metrics-tile/task.sh
@@ -4,4 +4,13 @@ chmod +x om-cli/om-linux
 
 FILE_PATH=`find ./pivnet-metrics-product -name *.pivotal`
 
+STEMCELL_VERSION=`cat ./pivnet-metrics-product/metadata.json | jq '.Dependencies[32].Release.Version'`
+
+echo "Downloading stemcell"
+pivnet-cli download-product-files -p stemcells -r $STEMCELL_VERSION -g "*vsphere*"
+
+SC_FILE_PATH=`find ./ -name *.tgz`
+
 ./om-cli/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH
+
+./om-cli/om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-stemcell -p $SC_FILE_PATH
