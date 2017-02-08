4364c618069c6bd605d816752384b176044ee17f
Rahul Jain <rjain@pivotal.io>
2017-02-08 14:53:07 -0600

    Adding a sanity check

--- tasks/delete-opsman/task.sh
+++ tasks/delete-opsman/task.sh
@@ -8,10 +8,6 @@ export GOVC_URL=$VCENTER_HOST
 export GOVC_USERNAME=$VCENTER_USR
 export GOVC_PASSWORD=$VCENTER_PWD
 
-PING_RESPONSE=`ping -c 1 -n $OPS_MGR_IP -i 1 | grep "100.0% packet loss"`
+set +e
 
-if [[ -z $VM_EXISTS ]]; then
-  ./govc/govc_linux_amd64 vm.destroy -vm.ip=$OPS_MGR_IP
-else
-  echo "$OPS_MGR_IP is down or does not exist"
-fi  
+./govc/govc_linux_amd64 vm.destroy -vm.ip=$OPS_MGR_IP
