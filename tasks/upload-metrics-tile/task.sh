c14d8a5c7a7bbb8ba21e2ea849ce8ad2209418bd
Rahul Jain <rjain@pivotal.io>
2017-02-08 15:29:21 -0600

    Fixed the missing command quote

--- tasks/upload-metrics-tile/task.sh
+++ tasks/upload-metrics-tile/task.sh
@@ -7,7 +7,7 @@ chmod +x om-cli/om-linux
 
 FILE_PATH=`find ./pivnet-metrics-product -name *.pivotal`
 
-STEMCELL_VERSION=`cat ./pivnet-metrics-product/metadata.json | jq '.Dependencies[] | select(.Release.Product.Name | contains("Stemcells")) | .Release.Version'
+STEMCELL_VERSION=`cat ./pivnet-metrics-product/metadata.json | jq '.Dependencies[] | select(.Release.Product.Name | contains("Stemcells")) | .Release.Version'`
 
 echo "Downloading stemcell $STEMCELL_VERSION"
 $PIVNET_CLI login --api-token="$PIVNET_API_TOKEN"
