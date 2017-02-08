d50a56acfe2b148fcd644b7443b8aced20a58416
Rahul Jain <rjain@pivotal.io>
2017-02-08 14:36:36 -0600

    Adding a sanity check

--- tasks/delete-products/task.sh
+++ tasks/delete-products/task.sh
@@ -2,4 +2,10 @@
 
 chmod +x om-cli/om-linux
 
-./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD delete-installation
+CURL_RESPONSE=`curl -I -s -L https://opsmgr.homelab.io | grep "HTTP/1.1" | grep "200 OK"`
+
+if [[ ! -z $CURL_RESPONSE ]]; then
+  ./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD delete-installation
+else
+  echo "Ops Manager not reachable or does not exist"  
+fi
