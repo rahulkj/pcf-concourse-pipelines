ab55d42fa74ef8dc8f8e766e79ca3cb243054b6b
Rahul Jain <rjain@pivotal.io>
2017-02-08 19:34:43 -0600

    Fixed the CF_GUID for disabling errands

--- tasks/disable-ert-errands/task.sh
+++ tasks/disable-ert-errands/task.sh
@@ -17,4 +17,6 @@ uaac target https://$OPS_MGR_HOST/uaa --skip-ssl-validation
 uaac token owner get opsman $OPS_MGR_USR -s "" -p $OPS_MGR_PWD
 UAA_ACCESS_TOKEN=`cat ~/.uaac.yml | grep "access_token" | cut -d ":" -f2 | tr -d " "`
 
-curl -k "https://$OPS_MGR_HOST/api/v0/staged/products/cf-7e4e34dcc3d9988f0a80/errands" -X PUT -H "Authorization: Bearer $UAA_ACCESS_TOKEN" -H "Content-Type: application/json" -d "$ERRANDS"
+CF_GUID=`curl "https://$OPS_MGR_HOST/api/v0/staged/products" -k -X GET -H "Authorization: Bearer $UAA_ACCESS_TOKEN" | jq '.[] | select(.installation_name | contains("cf-")) | .guid' | tr -d '"'`
+
+curl -k "https://$OPS_MGR_HOST/api/v0/staged/products/$CF_GUID/errands" -X PUT -H "Authorization: Bearer $UAA_ACCESS_TOKEN" -H "Content-Type: application/json" -d "$ERRANDS"
