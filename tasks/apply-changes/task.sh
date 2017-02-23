e814fee7528ad144cba605b36fa62b0c06708c06
Rahul Jain <rjain@pivotal.io>
2017-02-23 14:36:53 -0600

    Added the hook to latch on to the running apply-changes

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -2,3 +2,5 @@
 chmod +x om-cli/om-linux
 
 ./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/installations" -x POST -d '{ "ignore_warnings": "true" }'
+
+./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD apply-changes
