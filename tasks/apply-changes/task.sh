209181a25e7736a47fa14b1d5700fa43027f4322
Rahul Jain <rjain@pivotal.io>
2017-02-23 14:12:43 -0600

    Fixed a small glitch

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x om-cli/om-linux
 
-./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/staged/installations" -x POST -d '{ "ignore_warnings": "true" }'
+./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/installations" -x POST -d '{ "ignore_warnings": "true" }'
