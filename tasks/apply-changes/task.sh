5168951e142b7eef7fe15d0dddf203d1c7af8419
Rahul Jain <rjain@pivotal.io>
2017-02-23 13:53:44 -0600

    Updating the apply changes task

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x om-cli/om-linux
 
-./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD apply-changes
+./om-cli/om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/staged/installations" -x POST -d '{ "ignore_warnings": "true" }'
