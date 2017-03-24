d8e4bb393d8e7f0775d38bea179cc7a02616f13e
Rahul Jain <rahulkj@gmail.com>
2017-03-24 09:10:07 -0500

    Testing cert generation

--- tasks/config-ert/task.sh
+++ tasks/config-ert/task.sh
@@ -41,8 +41,8 @@ EOF
 
   CERTIFICATES=`$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -p "$OPS_MGR_GENERATE_SSL_ENDPOINT" -x POST -d "$DOMAINS"`
 
-  export SSL_CERT=`echo $CERTIFICATES | jq '.certificate'`
-  export SSL_PRIVATE_KEY=`echo $CERTIFICATES | jq '.key'`
+  export SSL_CERT=`echo $CERTIFICATES | jq '.certificate' | tr -d '"'`
+  export SSL_PRIVATE_KEY=`echo $CERTIFICATES | jq '.key' | tr -d '"'`
 
   echo "Using self signed certificates generated using Ops Manager..."
 
@@ -353,8 +353,8 @@ CF_SSL_TERM_PROPERTIES=$(cat <<-EOF
   },
   ".properties.networking_point_of_entry.haproxy.ssl_rsa_certificate": {
     "value": {
-      "cert_pem": $SSL_CERT,
-      "private_key_pem": $SSL_PRIVATE_KEY
+      "cert_pem": "$SSL_CERT",
+      "private_key_pem": "$SSL_PRIVATE_KEY"
     }
   }
 }
@@ -371,8 +371,8 @@ CF_SSL_TERM_PROPERTIES=$(cat <<-EOF
   },
   ".properties.networking_point_of_entry.external_ssl.ssl_rsa_certificate": {
     "value": {
-      "cert_pem": $SSL_CERT,
-      "private_key_pem": $SSL_PRIVATE_KEY
+      "cert_pem": "$SSL_CERT",
+      "private_key_pem": "$SSL_PRIVATE_KEY"
     }
   }
 }
