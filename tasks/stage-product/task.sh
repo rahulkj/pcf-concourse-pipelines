a0459edfa171987af65922e7afb916e177393e53
Rahul Jain <rahulkj@gmail.com>
2017-03-31 17:19:11 -0500

    Fixing the stage product task

--- tasks/stage-product/task.sh
+++ tasks/stage-product/task.sh
@@ -3,9 +3,6 @@
 chmod +x om-cli/om-linux
 CMD=./om-cli/om-linux
 
-RELEASE_NAME=`$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k available-products | grep $PRODUCT_IDENTIFIER`
+PRODUCT_VERSION=`echo pivnet-product/metadata.json | jq '.ProductFiles | .[] | select ( .File | contains("PCF Elastic Runtime")) | .FileVersion' | tr -d '"'`
 
-PRODUCT_NAME=`echo $RELEASE_NAME | cut -d"|" -f2 | tr -d " "`
-PRODUCT_VERSION=`echo $RELEASE_NAME | cut -d"|" -f3 | tr -d " "`
-
-$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_NAME -v $PRODUCT_VERSION
+$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_IDENTIFIER -v $PRODUCT_VERSION
