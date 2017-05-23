9910004d8fd1f34dbca256cea4d624e96dd1dfca
Rahul Jain <rahulkj@gmail.com>
2017-05-23 09:47:26 -0500

    fixing the stage product command

--- tasks/stage-product/task.sh
+++ tasks/stage-product/task.sh
@@ -10,4 +10,4 @@ RELEASE_NAME=`$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k a
 PRODUCT_NAME=`echo $RELEASE_NAME | cut -d"|" -f2 | tr -d " "`
 PRODUCT_VERSION=`echo $RELEASE_NAME | cut -d"|" -f3 | tr -d " "`
 
-$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_IDENTIFIER -v $PRODUCT_VERSION
+$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_NAME -v $PRODUCT_VERSION
