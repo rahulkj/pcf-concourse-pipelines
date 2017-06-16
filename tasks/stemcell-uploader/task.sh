9e1b2dd270abae16d8291724881f733e6af8a992
Rahul Jain <rahulkj@gmail.com>
2017-06-16 16:05:41 -0500

    fixing upload stemcell task

--- tasks/stemcell-uploader/task.sh
+++ tasks/stemcell-uploader/task.sh
@@ -12,17 +12,19 @@ CMD=./om-cli/om-linux
 
 STEMCELL_VERSION=`cat ./pivnet-product/metadata.json | jq '.Dependencies[] | select(.Release.Product.Name | contains("Stemcells")) | .Release.Version'`
 
-echo "Downloading stemcell $STEMCELL_VERSION"
+SC_VERSION=`echo $STEMCELL_VERSION \ tr -d '"'`
+
+echo "Downloading stemcell $SC_VERSION"
 $PIVNET_CLI login --api-token="$PIVNET_API_TOKEN"
 
 set +e
-RESPONSE=`$PIVNET_CLI releases -p stemcells | grep $STEMCELL_VERSION`
+RESPONSE=`$PIVNET_CLI releases -p stemcells | grep $SC_VERSION`
 set -e
 
 if [[ -z "$RESPONSE" ]]; then
-  wget --show-progress https://s3.amazonaws.com/bosh-core-stemcells/vsphere/bosh-stemcell-$STEMCELL_VERSION-$IAAS_TYPE-esxi-ubuntu-trusty-go_agent.tgz
+  wget --show-progress https://s3.amazonaws.com/bosh-core-stemcells/vsphere/bosh-stemcell-$SC_VERSION-$IAAS_TYPE-esxi-ubuntu-trusty-go_agent.tgz
 else
-  $PIVNET_CLI download-product-files -p stemcells -r $STEMCELL_VERSION -g "*$IAAS_TYPE*" --accept-eula
+  $PIVNET_CLI download-product-files -p stemcells -r $SC_VERSION -g "*$IAAS_TYPE*" --accept-eula
 fi
 
 SC_FILE_PATH=`find ./ -name *.tgz`
@@ -32,6 +34,6 @@ $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-stemcell
 if [ ! -f "$SC_FILE_PATH" ]; then
     echo "Stemcell file not found!"
 else
-  echo "Removing downloaded stemcell $STEMCELL_VERSION"
+  echo "Removing downloaded stemcell $SC_VERSION"
   rm $SC_FILE_PATH
 fi
