412f9902b5d9c665a5ab2ca0037a87793ad12487
Rahul Jain <rahulkj@gmail.com>
2017-06-16 14:51:22 -0500

    Fixing the copy paste error

--- tasks/replicate-and-upload-tile/task.sh
+++ tasks/replicate-and-upload-tile/task.sh
@@ -4,7 +4,7 @@ chmod +x replicator/replicator-linux
 CMD=./replicator/replicator-linux
 
 INPUT_FILE_PATH=`find ./pivnet-product -name "*.pivotal"`
-FILE_NAME=`echo $FILE_PATH | cut -d '/' -f3`
+FILE_NAME=`echo $INPUT_FILE_PATH | cut -d '/' -f3`
 OUTPUT_FILE_PATH=replicator-tile/$FILE_NAME
 
 $CMD -name $REPLICATOR_NAME -path $INPUT_FILE_PATH -output $OUTPUT_FILE_PATH
