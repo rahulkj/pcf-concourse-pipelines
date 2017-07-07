7e4bcaaa9b1b36d9de3792829040147a716db510
Rahul Jain <rahulkj@gmail.com>
2017-07-07 16:42:47 -0500

    Cleaned up the fly command download

--- tasks/set-upgrade-pipeline/task.sh
+++ tasks/set-upgrade-pipeline/task.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 
-wget https://github.com/concourse/concourse/releases/download/$(cat fly-cli/tag)/fly_linux_amd64
+ curl "http://$ATC_EXTERNAL_URL/api/v1/cli?arch=amd64&platform=linux" > fly
 
-FLY_CLI=$(find . -name "*linux_amd64*")
+FLY_CLI=$(find . -name "fly")
 chmod +x $FLY_CLI
 
 $FLY_CLI -t cc login -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $BUILD_TEAM_NAME
