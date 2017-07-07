13941093ef46369cf26f03f6a5f51f6fbe47548d
Rahul Jain <rahulkj@gmail.com>
2017-07-07 01:31:21 -0500

    use fly cli

--- tasks/set-upgrade-pipeline/task.sh
+++ tasks/set-upgrade-pipeline/task.sh
@@ -5,7 +5,7 @@ wget https://github.com/concourse/concourse/releases/download/$(cat fly-cli/tag)
 FLY_CLI=$(find . -name "*linux_amd64*")
 chmod +x $FLY_CLI
 
-$FLY_CLI -t cc login -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $BUILD_TEAM_NAME
+$FLY_CLI -t cc login -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $BUILD_TEAM_NAME
 $FLY_CLI -t cc set-pipeline -p $UPGRADE_PIPELINE_NAME -c concourse-vsphere/pipelines/upgrade-tile/pipeline.yml
 $FLY_CLI -t cc unpause-pipeline -p $UPGRADE_PIPELINE_NAME
 $FLY_CLI -t cc pause-pipeline -p $BUILD_PIPELINE_NAME
