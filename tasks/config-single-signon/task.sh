ae47c209315967d4ca3076c689b4fec2215f75dc
Rahul Jain <rahulkj@gmail.com>
2017-08-01 09:54:41 -0500

    Adding Single Signon Tile pipeline

---
+++ tasks/config-single-signon/task.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -ex
+
+chmod +x om-cli/om-linux
+CMD=./om-cli/om-linux
+
+function fn_other_azs {
+  local azs_csv=$1
+  echo $azs_csv | awk -F "," -v braceopen='{' -v braceclose='}' -v name='"name":' -v quote='"' -v OFS='"},{"name":"' '$1=$1 {print braceopen name quote $0 quote braceclose}'
+}
+
+BALANCE_JOB_AZS=$(fn_other_azs $OTHER_AZS)
+
+PRODUCT_NETWORK_CONFIG=$(cat <<-EOF
+{
+  "singleton_availability_zone": {
+    "name": "$SINGLETON_JOB_AZ"
+  },
+  "other_availability_zones": [
+    $BALANCE_JOB_AZS
+  ],
+  "network": {
+    "name": "$NETWORK_NAME"
+  }
+}
+EOF
+)
+
+PRODUCT_RESOURCE_CONFIG=$(cat <<-EOF
+{
+  "deploy-service-broker": {
+    "instance_type": {"id": "automatic"},
+    "instances": $DEPLOY_SERVICE_BROKER_INSTANCES
+  }
+}
+EOF
+)
+
+$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_IDENTIFIER -pn "$PRODUCT_NETWORK_CONFIG"
+$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_IDENTIFIER -p "$PRODUCT_PROPERTIES"  -pr "$PRODUCT_RESOURCE_CONFIG"
