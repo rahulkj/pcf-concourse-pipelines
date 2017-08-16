5553df71f79d22d3d893557b1745d994005b6ab0
Rahul Jain <rahulkj@gmail.com>
2017-08-16 10:33:01 -0500

    Fixing the if loop

--- tasks/update-metrics-ip/task.sh
+++ tasks/update-metrics-ip/task.sh
@@ -7,9 +7,9 @@ METRICS_GUID=`$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD c
 
 METRICS_MANIFEST=`$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/staged/products/$METRICS_GUID/manifest" -x GET`
 
-if [[ "NAT_ENABLED" == "option_enabled" ]]; then
+if [[ "$NAT_ENABLED" == "option_enabled" ]]; then
   MAXIMUS_IP=`echo $METRICS_MANIFEST | jq '.manifest.instance_groups[] | select(.name | contains("maximus")) | .properties.maximus.public_hostname' | tr -d '"'`
-elif [[ "NAT_ENABLED" == "option_disabled" ]]; then
+elif [[ "$NAT_ENABLED" == "option_disabled" ]]; then
   MAXIMUS_IP=`echo $METRICS_MANIFEST | jq '.manifest.instance_groups[] | select(.name | contains("maximus")) | .networks | .[] | .static_ips | .[]' | tr -d '"'`
 fi
 
