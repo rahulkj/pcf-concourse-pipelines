8adfea9cc272e82f4c56b1fc0c475d894feddda7
Rahul Jain <rahulkj@gmail.com>
2017-08-16 10:31:43 -0500

    Fixing the format

--- tasks/update-metrics-ip/task.sh
+++ tasks/update-metrics-ip/task.sh
@@ -9,7 +9,7 @@ METRICS_MANIFEST=`$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_P
 
 if [[ "NAT_ENABLED" == "option_enabled" ]]; then
   MAXIMUS_IP=`echo $METRICS_MANIFEST | jq '.manifest.instance_groups[] | select(.name | contains("maximus")) | .properties.maximus.public_hostname' | tr -d '"'`
-elif [[ "NAT_ENABLED" == "option_disabled" ]]
+elif [[ "NAT_ENABLED" == "option_disabled" ]]; then
   MAXIMUS_IP=`echo $METRICS_MANIFEST | jq '.manifest.instance_groups[] | select(.name | contains("maximus")) | .networks | .[] | .static_ips | .[]' | tr -d '"'`
 fi
 
