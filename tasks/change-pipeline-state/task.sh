c04545a2d88c915837e3b9df8a63d43c44221336
Rahul Jain <rahulkj@gmail.com>
2017-08-25 22:14:57 -0500

    Updating the task to change state of a pipeline

--- tasks/change-pipeline-state/task.sh
+++ tasks/change-pipeline-state/task.sh
@@ -6,8 +6,5 @@ FLY_CLI=$(find . -name "fly")
 chmod +x $FLY_CLI
 
 $FLY_CLI -t cc login -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $TEAM_NAME
-if [[ "$PIPELINE_STATE" == "unpause" ]]; then
-  $FLY_CLI -t cc unpause-pipeline -p $PIPELINE_NAME
-elif [[ "$PIPELINE_STATE" == "pause" ]]; then
-  $FLY_CLI -t cc pause-pipeline -p $PIPELINE_NAME
-fi
+
+$FLY_CLI -t cc $PIPELINE_STATE-pipeline -p $PIPELINE_NAME
