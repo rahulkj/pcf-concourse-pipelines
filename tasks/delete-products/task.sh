afe5b6359e8ef2daf73a2e1a3afe3706f41f6646
Rahul Jain <rahulkj@gmail.com>
2017-09-10 14:52:19 -0500

    Using om cli to delete installation

--- tasks/delete-products/task.sh
+++ tasks/delete-products/task.sh
@@ -3,4 +3,4 @@
 chmod +x om-cli/om-linux
 CMD=./om-cli/om-linux
 
-$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -p "/api/v0/installation_asset_collection" -x DELETE -d '{}'
+$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD delete-installation
