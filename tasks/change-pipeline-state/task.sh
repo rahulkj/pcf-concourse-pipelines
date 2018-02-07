e8d0cf877b4406a6a89ddf73fab189bb68c751ae
rahulkj <rahulkj@gmail.com>
2018-02-07 13:55:23 -0600

    Adding skip ssl while change state of the pipeline

--- tasks/change-pipeline-state/task.sh
+++ tasks/change-pipeline-state/task.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -ex
 
- curl "$ATC_EXTERNAL_URL/api/v1/cli?arch=amd64&platform=linux" > fly
+ curl "$ATC_EXTERNAL_URL/api/v1/cli?arch=amd64&platform=linux" -k > fly
 
 FLY_CLI=$(find . -name "fly")
 chmod +x $FLY_CLI
 
-$FLY_CLI -t cc login -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $TEAM_NAME
+$FLY_CLI -t cc login -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $TEAM_NAME -k
 
 $FLY_CLI -t cc $PIPELINE_STATE-pipeline -p $PIPELINE_NAME
