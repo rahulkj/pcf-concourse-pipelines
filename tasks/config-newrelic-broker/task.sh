139b9a27e905578e0673f8023477e72b7a527c42
rahulkj <rahulkj@gmail.com>
2018-04-08 00:20:52 -0500

    Adding pipelines with some default values

--- tasks/config-newrelic-broker/task.sh
+++ tasks/config-newrelic-broker/task.sh
@@ -14,16 +14,14 @@ JQ_CMD=./jq/jq-linux64
 
 properties_config=$($JQ_CMD -n \
   --arg allow_paid_service_plans ${ALLOW_PAID_SERVICE_PLANS:-false} \
+  --arg app_name ${APP_NAME:-"NewRelicServiceBroker"} \
+  --arg app_uri ${APP_URI:-"newrelic-broker"} \
+  --arg app_version ${APP_VERSION:-"v1.12.9"} \
   --arg apply_open_security_group ${APPLY_OPEN_SECURITY_GROUP:-false} \
-  --arg dataflow_db_plan ${DATAFLOW_DB_PLAN:-"db-small"} \
-  --arg dataflow_db_service ${DATAFLOW_DB_SERVICE:-"p.mysql"} \
-  --arg db_service_name ${DB_SERVICE_NAME:-"p.mysql"} \
-  --arg db_service_plan ${DB_SERVICE_PLAN:-"db-small"} \
-  --arg org ${ORG:-"system"} \
-  --arg p_dataflow_enable_global_access_to_plans ${P_DATAFLOW_ENABLE_GLOBAL_ACCESS_TO_PLANS:-true} \
-  --arg skipper_db_plan ${SKIPPER_DB_PLAN:-"db-small"} \
-  --arg skipper_db_service ${SKIPPER_DB_SERVICE:-"p.mysql"} \
-  --arg space ${SPACE:-"p-dataflow"} \
+  --arg newrelic_broker_enable_global_access_to_plans ${NEWRELIC_BROKER_ENABLE_GLOBAL_ACCESS_TO_PLANS:-true} \
+  --arg nrplans ${NRPLANS} \
+  --arg org ${ORG:-"NewRelicServiceBroker-service-org"} \
+  --arg space ${SPACE:-"NewRelicServiceBroker-service-space"} \
 '{
   ".properties.org": {
     "value": $org
@@ -37,26 +35,20 @@ properties_config=$($JQ_CMD -n \
   ".properties.allow_paid_service_plans": {
     "value": $allow_paid_service_plans
   },
-  ".properties.p_dataflow_enable_global_access_to_plans": {
-    "value": $p_dataflow_enable_global_access_to_plans
+  ".properties.newrelic_broker_enable_global_access_to_plans": {
+    "value": $newrelic_broker_enable_global_access_to_plans
   },
-  ".properties.db_service_name": {
-    "value": $db_service_name
+  ".properties.app_name": {
+    "value": $app_name
   },
-  ".properties.db_service_plan": {
-    "value": $db_service_plan
+  ".properties.app_version": {
+    "value": $app_version
   },
-  ".properties.dataflow_db_service": {
-    "value": $dataflow_db_service
+  ".properties.app_uri": {
+    "value": $app_uri
   },
-  ".properties.dataflow_db_plan": {
-    "value": $dataflow_db_plan
-  },
-  ".properties.skipper_db_service": {
-    "value": $skipper_db_service
-  },
-  ".properties.skipper_db_plan": {
-    "value": $skipper_db_plan
+  ".properties.nrplans": {
+    "value": [$nrplans]
   }
 }'
 )
@@ -64,7 +56,7 @@ properties_config=$($JQ_CMD -n \
 network_config=$($JQ_CMD -n \
   --arg network_name "$NETWORK_NAME" \
   --arg other_azs "$OTHER_AZS" \
-  --arg singleton_az "$SINGLETON_JOBS_AZ" \
+  --arg singleton_az "$SINGLETON_JOBS_AZ"
 '
   {
     "network": {
@@ -84,6 +76,6 @@ $OM_CMD \
   --password "$OPS_MGR_PWD" \
   --skip-ssl-validation \
   configure-product \
-  --product-name p-dataflow \
+  --product-name newrelic-broker \
   --product-properties "$properties_config" \
   --product-network "$network_config"
--- tasks/config-splunk-nozzle/task.sh
+++ tasks/config-splunk-nozzle/task.sh
@@ -12,92 +12,92 @@ OM_CMD=./om-cli/om-linux
 chmod +x ./jq/jq-linux64
 JQ_CMD=./jq/jq-linux64
 
-config=$($JQ_CMD -n \
-  --argjson add_app_info ${ADD_APP_INFO:-false} \
-  --argjson allow_paid_service_plans ${ALLOW_PAID_SERVICE_PLANS:-true} \
-  --arg api_endpoint $API_ENDPOINT \
-  --arg api_password $API_PASSWORD \
-  --arg api_user $API_USER \
-  --argjson apply_open_security_group ${APPLY_OPEN_SECURITY_GROUP:-true} \
-  --argjson enable_event_tracing ${ENABLE_EVENT_TRACING:-false} \
-  --arg events $EVENTS \
-  --arg extra_fields $EXTRA_FIELDS \
-  --arg firehose_subscription_id $FIREHOSE_SUBSCRIPTION_ID \
+properties_config=$($JQ_CMD -n \
+  --arg add_app_info ${ADD_APP_INFO:-false} \
+  --arg allow_paid_service_plans ${ALLOW_PAID_SERVICE_PLANS:-true} \
+  --arg api_endpoint ${API_ENDPOINT:-null} \
+  --arg api_password ${API_PASSWORD:-null} \
+  --arg api_user ${API_USER:-null} \
+  --arg apply_open_security_group ${APPLY_OPEN_SECURITY_GROUP:-true} \
+  --arg enable_event_tracing ${ENABLE_EVENT_TRACING:-false} \
+  --arg events ${EVENTS:-null} \
+  --arg extra_fields ${EXTRA_FIELDS:-null} \
+  --arg firehose_subscription_id ${FIREHOSE_SUBSCRIPTION_ID:-null} \
   --arg org ${ORG:-"splunk-nozzle-org"} \
-  --argjson scale_out_nozzle ${SCALE_OUT_NOZZLE:-2} \
-  --argjson skip_ssl_validation_cf ${SKIP_SSL_VALIDATION_CF:-false} \
-  --argjson skip_ssl_validation_splunk ${SKIP_SSL_VALIDATION_SPLUNK:-false} \
+  --arg scale_out_nozzle ${SCALE_OUT_NOZZLE:-2} \
+  --arg skip_ssl_validation_cf ${SKIP_SSL_VALIDATION_CF:-false} \
+  --arg skip_ssl_validation_splunk ${SKIP_SSL_VALIDATION_SPLUNK:-false} \
   --arg space ${SPACE:-"splunk-nozzle-space"} \
-  --arg splunk_host $SPLUNK_HOST \
-  --arg splunk_index $SPLUNK_INDEX \
-  --arg splunk_token $SPLUNK_TOKEN \
-  '{
-    ".properties.org": {
-      "value": $org
-    },
-    ".properties.space": {
-      "value": $space
-    },
-    ".properties.apply_open_security_group": {
-      "value": $apply_open_security_group
-    },
-    ".properties.allow_paid_service_plans": {
-      "value": $allow_paid_service_plans
-    },
-    ".properties.splunk_host": {
-      "value": $splunk_host
-    },
-    ".properties.splunk_token": {
-      "value": {
-        "secret": $splunk_token
-      }
-    },
-    ".properties.skip_ssl_validation_splunk": {
-      "value": $skip_ssl_validation_splunk
-    },
-    ".properties.splunk_index": {
-      "value": $splunk_index
-    },
-    ".properties.api_endpoint": {
-      "value": $api_endpoint
-    },
-    ".properties.api_user": {
-      "value": $api_user
-    },
-    ".properties.api_password": {
-      "value": {
-        "secret": $api_password
-      }
-    },
-    ".properties.skip_ssl_validation_cf": {
-      "value": $skip_ssl_validation_cf
-    },
-    ".properties.events": {
-      "value": ($events | split(","))
-    },
-    ".properties.scale_out_nozzle": {
-      "value": $scale_out_nozzle
-    },
-    ".properties.firehose_subscription_id": {
-      "value": $firehose_subscription_id
-    },
-    ".properties.extra_fields": {
-      "value": $extra_fields
-    },
-    ".properties.add_app_info": {
-      "value": $add_app_info
-    },
-    ".properties.enable_event_tracing": {
-      "value": $enable_event_tracing
+  --arg splunk_host ${SPLUNK_HOST:-null} \
+  --arg splunk_index ${SPLUNK_INDEX:-"main"} \
+  --arg splunk_token ${SPLUNK_TOKEN:-null} \
+'{
+  ".properties.org": {
+    "value": $org
+  },
+  ".properties.space": {
+    "value": $space
+  },
+  ".properties.apply_open_security_group": {
+    "value": $apply_open_security_group
+  },
+  ".properties.allow_paid_service_plans": {
+    "value": $allow_paid_service_plans
+  },
+  ".properties.splunk_host": {
+    "value": $splunk_host
+  },
+  ".properties.splunk_token": {
+    "value": {
+      "secret": $splunk_token
     }
-  }'
+  },
+  ".properties.skip_ssl_validation_splunk": {
+    "value": $skip_ssl_validation_splunk
+  },
+  ".properties.splunk_index": {
+    "value": $splunk_index
+  },
+  ".properties.api_endpoint": {
+    "value": $api_endpoint
+  },
+  ".properties.api_user": {
+    "value": $api_user
+  },
+  ".properties.api_password": {
+    "value": {
+      "secret": $api_password
+    }
+  },
+  ".properties.skip_ssl_validation_cf": {
+    "value": $skip_ssl_validation_cf
+  },
+  ".properties.events": {
+    "value": $events
+  },
+  ".properties.scale_out_nozzle": {
+    "value": $scale_out_nozzle
+  },
+  ".properties.firehose_subscription_id": {
+    "value": $firehose_subscription_id
+  },
+  ".properties.extra_fields": {
+    "value": $extra_fields
+  },
+  ".properties.add_app_info": {
+    "value": $add_app_info
+  },
+  ".properties.enable_event_tracing": {
+    "value": $enable_event_tracing
+  }
+}'
 )
 
 network_config=$($JQ_CMD -n \
   --arg network_name "$NETWORK_NAME" \
   --arg other_azs "$OTHER_AZS" \
   --arg singleton_az "$SINGLETON_JOBS_AZ" \
-  '
+'
   {
     "network": {
       "name": $network_name
@@ -107,15 +107,15 @@ network_config=$($JQ_CMD -n \
       "name": $singleton_az
     }
   }
-  '
+'
 )
 
 $OM_CMD \
-  --target https://$OPS_MGR_PWD \
+  --target https://$OPS_MGR_HOST \
   --username "$OPS_MGR_USR" \
   --password "$OPS_MGR_PWD" \
   --skip-ssl-validation \
   configure-product \
   --product-name splunk-nozzle \
-  --product-properties "$config" \
+  --product-properties "$properties_config" \
   --product-network "$network_config"
