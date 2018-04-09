462e79cab6596d8b96819d333fe8ea1e9be66f6a
rahulkj <rahulkj@gmail.com>
2018-04-09 09:34:18 -0400

    Renaming ATC_EXTERNAL_URL to CONCOURSE_URL

--- tasks/set-upgrade-pipeline/task.sh
+++ tasks/set-upgrade-pipeline/task.sh
@@ -6,12 +6,12 @@ else
   set -e
 fi
 
-curl -k "$ATC_EXTERNAL_URL/api/v1/cli?arch=amd64&platform=linux" > fly
+curl -k "$CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux" > fly
 
 FLY_CLI=$(find . -name "fly")
 chmod +x $FLY_CLI
 
-$FLY_CLI -t cc login -k -c $ATC_EXTERNAL_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $BUILD_TEAM_NAME
+$FLY_CLI -t cc login -k -c $CONCOURSE_URL -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWD -n $BUILD_TEAM_NAME
 $FLY_CLI -t cc set-pipeline -n -p $UPGRADE_PIPELINE_NAME -c pipelines-repo/pipelines/upgrade-tile/pipeline.yml \
   -l pipelines-repo/pipelines/upgrade-tile/params.yml \
   -v product_name="$PRODUCT_NAME" \
