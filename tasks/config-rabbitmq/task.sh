50c009a1fa1079f8c1f45acfa2d00bdd6f49612b
rahulkj <rahulkj@gmail.com>
2018-04-10 13:00:29 -0400

    Updating RMQ tasks - wrapping vars with quotes

--- tasks/config-rabbitmq/task.sh
+++ tasks/config-rabbitmq/task.sh
@@ -13,59 +13,59 @@ chmod +x ./jq/jq-linux64
 JQ_CMD=./jq/jq-linux64
 
 properties_config=$($JQ_CMD -n \
-  --arg on_demand_broker_global_service_instance_quota ${ON_DEMAND_BROKER_GLOBAL_SERVICE_INSTANCE_QUOTA:-20} \
-  --arg disk_alarm_threshold ${DISK_ALARM_THRESHOLD:-"mem_relative_1_5"} \
-  --arg metrics_polling_interval ${METRICS_POLLING_INTERVAL:-30} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR:-"disabled"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_cf_service_access ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_CF_SERVICE_ACCESS:-"enable"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_description ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_DESCRIPTION:-"This plan provides a preconfigured dedicated cluster of RabbitMQ nodes"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_disk_limit_acknowledgement ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_DISK_LIMIT_ACKNOWLEDGEMENT:-"non-existant-value"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_features ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_FEATURES:-"RabbitMQ"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_instance_quota ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_INSTANCE_QUOTA:-0} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_name ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_NAME:-"cluster"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_az_placement ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_AZ_PLACEMENT} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_cluster_partition_handling_strategy ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_CLUSTER_PARTITION_HANDLING_STRATEGY:-"pause_minority"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_number_of_nodes ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_NUMBER_OF_NODES:-3} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_persistent_disk_type ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_PERSISTENT_DISK_TYPE:-"1024"} \
-  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_vm_type ${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_VM_TYPE:-"nano"} \
-  --arg on_demand_broker_dedicated_single_node_plan_cf_service_access ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_CF_SERVICE_ACCESS:-"disable"} \
-  --arg on_demand_broker_dedicated_single_node_plan_description ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_DESCRIPTION:-"This plan provides a single dedicated RabbitMQ node"} \
-  --arg on_demand_broker_dedicated_single_node_plan_disk_limit_acknowledgement ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_DISK_LIMIT_ACKNOWLEDGEMENT:-"non-existant-value"} \
-  --arg on_demand_broker_dedicated_single_node_plan_features ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_FEATURES:-"RabbitMQ"} \
-  --arg on_demand_broker_dedicated_single_node_plan_instance_quota ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_INSTANCE_QUOTA:-0} \
-  --arg on_demand_broker_dedicated_single_node_plan_name ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_NAME:-"solo"} \
-  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_az_placement ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_AZ_PLACEMENT} \
-  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_persistent_disk_type ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_PERSISTENT_DISK_TYPE:-"1024"} \
-  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_vm_type ${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_VM_TYPE:-"nano"} \
-  --arg on_demand_broker_vm_extensions ${ON_DEMAND_BROKER_VM_EXTENSIONS:-""} \
-  --arg syslog_selector ${SYSLOG_SELECTOR:-"enabled"} \
-  --arg syslog_selector_enabled_address ${SYSLOG_SELECTOR_ENABLED_ADDRESS} \
-  --arg syslog_selector_enabled_port ${SYSLOG_SELECTOR_ENABLED_PORT} \
-  --arg syslog_selector_enabled_syslog_ca_cert ${SYSLOG_SELECTOR_ENABLED_SYSLOG_CA_CERT} \
-  --arg syslog_selector_enabled_syslog_format ${SYSLOG_SELECTOR_ENABLED_SYSLOG_FORMAT:-"rfc5424"} \
-  --arg syslog_selector_enabled_syslog_permitted_peer ${SYSLOG_SELECTOR_ENABLED_SYSLOG_PERMITTED_PEER} \
-  --arg syslog_selector_enabled_syslog_tls ${SYSLOG_SELECTOR_ENABLED_SYSLOG_TLS:-false} \
-  --arg syslog_selector_enabled_syslog_transport ${SYSLOG_SELECTOR_ENABLED_SYSLOG_TRANSPORT:-"tcp"} \
-  --arg rabbitmq_broker_dns_host ${RABBITMQ_BROKER_DNS_HOST} \
-  --arg rabbitmq_broker_operator_set_policy_enabled ${RABBITMQ_BROKER_OPERATOR_SET_POLICY_ENABLED:-false} \
-  --arg rabbitmq_broker_policy_definition ${RABBITMQ_BROKER_POLICY_DEFINITION} \
-  --arg rabbitmq_haproxy_static_ips ${RABBITMQ_HAPROXY_STATIC_IPS} \
-  --arg rabbitmq_server_cluster_partition_handling ${RABBITMQ_SERVER_CLUSTER_PARTITION_HANDLING:-"pause_minority"} \
-  --arg rabbitmq_server_config ${RABBITMQ_SERVER_CONFIG} \
-  --arg rabbitmq_server_cookie ${RABBITMQ_SERVER_COOKIE} \
-  --arg rabbitmq_server_plugins ${RABBITMQ_SERVER_PLUGINS:-'rabbitmq_management'} \
-  --arg rabbitmq_server_ports ${RABBITMQ_SERVER_PORTS:-"15672, 5672, 5671, 1883, 8883, 61613, 61614, 15674"} \
-  --arg rabbitmq_server_ssl_cert_pem ${RABBITMQ_SERVER_SSL_CERT_PEM} \
-  --arg rabbitmq_server_ssl_private_key_pem ${RABBITMQ_SERVER_SSL_PRIVATE_KEY_PEM} \
-  --arg rabbitmq_server_rsa_certificate ${RABBITMQ_SERVER_RSA_CERTIFICATE} \
-  --arg rabbitmq_server_server_admin_username ${RABBITMQ_SERVER_SERVER_ADMIN_USERNAME} \
-  --arg rabbitmq_server_server_admin_password ${RABBITMQ_SERVER_SERVER_ADMIN_PASSWORD} \
-  --arg rabbitmq_server_ssl_cacert ${RABBITMQ_SERVER_SSL_CACERT} \
-  --arg rabbitmq_server_ssl_fail_if_no_peer_cert ${RABBITMQ_SERVER_SSL_FAIL_IF_NO_PEER_CERT:-false} \
-  --arg rabbitmq_server_ssl_verification_depth ${RABBITMQ_SERVER_SSL_VERIFICATION_DEPTH:-5} \
-  --arg rabbitmq_server_ssl_verify ${RABBITMQ_SERVER_SSL_VERIFY:-false} \
-  --arg rabbitmq_server_ssl_versions ${RABBITMQ_SERVER_SSL_VERSIONS:-"tlsv1.1,tlsv1.2"} \
-  --arg rabbitmq_server_static_ips ${RABBITMQ_SERVER_STATIC_IPS:-""} \
+  --arg on_demand_broker_global_service_instance_quota "${ON_DEMAND_BROKER_GLOBAL_SERVICE_INSTANCE_QUOTA:-20}" \
+  --arg disk_alarm_threshold "${DISK_ALARM_THRESHOLD:-"mem_relative_1_5"}" \
+  --arg metrics_polling_interval "${METRICS_POLLING_INTERVAL:-30}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR:-"disabled"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_cf_service_access "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_CF_SERVICE_ACCESS:-"enable"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_description "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_DESCRIPTION:-"This plan provides a preconfigured dedicated cluster of RabbitMQ nodes"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_disk_limit_acknowledgement "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_DISK_LIMIT_ACKNOWLEDGEMENT:-"non-existant-value"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_features "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_FEATURES:-"RabbitMQ"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_instance_quota "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_INSTANCE_QUOTA:-0}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_name "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_NAME:-"cluster"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_az_placement "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_AZ_PLACEMENT}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_cluster_partition_handling_strategy "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_CLUSTER_PARTITION_HANDLING_STRATEGY:-"pause_minority"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_number_of_nodes "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_NUMBER_OF_NODES:-3}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_persistent_disk_type "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_PERSISTENT_DISK_TYPE:-"1024"}" \
+  --arg on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_vm_type "${ON_DEMAND_BROKER_DEDICATED_CLUSTER_PLAN_SELECTOR_ENABLED_RABBITMQ_VM_TYPE:-"nano"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_cf_service_access "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_CF_SERVICE_ACCESS:-"disable"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_description "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_DESCRIPTION:-"This plan provides a single dedicated RabbitMQ node"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_disk_limit_acknowledgement "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_DISK_LIMIT_ACKNOWLEDGEMENT:-"non-existant-value"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_features "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_FEATURES:-"RabbitMQ"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_instance_quota "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_INSTANCE_QUOTA:-0}" \
+  --arg on_demand_broker_dedicated_single_node_plan_name "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_NAME:-"solo"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_az_placement "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_AZ_PLACEMENT}" \
+  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_persistent_disk_type "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_PERSISTENT_DISK_TYPE:-"1024"}" \
+  --arg on_demand_broker_dedicated_single_node_plan_rabbitmq_vm_type "${ON_DEMAND_BROKER_DEDICATED_SINGLE_NODE_PLAN_RABBITMQ_VM_TYPE:-"nano"}" \
+  --arg on_demand_broker_vm_extensions "${ON_DEMAND_BROKER_VM_EXTENSIONS}" \
+  --arg syslog_selector "${SYSLOG_SELECTOR:-"enabled"}" \
+  --arg syslog_selector_enabled_address "${SYSLOG_SELECTOR_ENABLED_ADDRESS}" \
+  --arg syslog_selector_enabled_port "${SYSLOG_SELECTOR_ENABLED_PORT}" \
+  --arg syslog_selector_enabled_syslog_ca_cert "${SYSLOG_SELECTOR_ENABLED_SYSLOG_CA_CERT}" \
+  --arg syslog_selector_enabled_syslog_format "${SYSLOG_SELECTOR_ENABLED_SYSLOG_FORMAT:-"rfc5424"}" \
+  --arg syslog_selector_enabled_syslog_permitted_peer "${SYSLOG_SELECTOR_ENABLED_SYSLOG_PERMITTED_PEER}" \
+  --arg syslog_selector_enabled_syslog_tls "${SYSLOG_SELECTOR_ENABLED_SYSLOG_TLS:-false}" \
+  --arg syslog_selector_enabled_syslog_transport "${SYSLOG_SELECTOR_ENABLED_SYSLOG_TRANSPORT:-"tcp"}" \
+  --arg rabbitmq_broker_dns_host "${RABBITMQ_BROKER_DNS_HOST}" \
+  --arg rabbitmq_broker_operator_set_policy_enabled "${RABBITMQ_BROKER_OPERATOR_SET_POLICY_ENABLED:-false}" \
+  --arg rabbitmq_broker_policy_definition "${RABBITMQ_BROKER_POLICY_DEFINITION}" \
+  --arg rabbitmq_haproxy_static_ips ""${RABBITMQ_HAPROXY_STATIC_IPS}"" \
+  --arg rabbitmq_server_cluster_partition_handling "${RABBITMQ_SERVER_CLUSTER_PARTITION_HANDLING:-"pause_minority"}" \
+  --arg rabbitmq_server_config "${RABBITMQ_SERVER_CONFIG}" \
+  --arg rabbitmq_server_cookie "${RABBITMQ_SERVER_COOKIE}" \
+  --arg rabbitmq_server_plugins "${RABBITMQ_SERVER_PLUGINS:-'rabbitmq_management'}" \
+  --arg rabbitmq_server_ports "${RABBITMQ_SERVER_PORTS:-"15672, 5672, 5671, 1883, 8883, 61613, 61614, 15674"}" \
+  --arg rabbitmq_server_ssl_cert_pem "${RABBITMQ_SERVER_SSL_CERT_PEM}" \
+  --arg rabbitmq_server_ssl_private_key_pem "${RABBITMQ_SERVER_SSL_PRIVATE_KEY_PEM}" \
+  --arg rabbitmq_server_rsa_certificate "${RABBITMQ_SERVER_RSA_CERTIFICATE}" \
+  --arg rabbitmq_server_server_admin_username "${RABBITMQ_SERVER_SERVER_ADMIN_USERNAME}" \
+  --arg rabbitmq_server_server_admin_password "${RABBITMQ_SERVER_SERVER_ADMIN_PASSWORD}" \
+  --arg rabbitmq_server_ssl_cacert "${RABBITMQ_SERVER_SSL_CACERT}" \
+  --arg rabbitmq_server_ssl_fail_if_no_peer_cert "${RABBITMQ_SERVER_SSL_FAIL_IF_NO_PEER_CERT:-false}" \
+  --arg rabbitmq_server_ssl_verification_depth "${RABBITMQ_SERVER_SSL_VERIFICATION_DEPTH:-5}" \
+  --arg rabbitmq_server_ssl_verify "${RABBITMQ_SERVER_SSL_VERIFY:-false}" \
+  --arg rabbitmq_server_ssl_versions "${RABBITMQ_SERVER_SSL_VERSIONS:-"tlsv1.1,tlsv1.2"}" \
+  --arg rabbitmq_server_static_ips "${RABBITMQ_SERVER_STATIC_IPS:-""}" \
 '{
   ".properties.metrics_polling_interval": {
     "value": $metrics_polling_interval
