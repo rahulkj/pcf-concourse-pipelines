e796b66fc54e66b5859db72e04f121c08f067fa3
rahulkj <rahulkj@gmail.com>
2018-04-10 13:22:46 -0400

    fixing ack araay

--- tasks/config-rabbitmq/task.sh
+++ tasks/config-rabbitmq/task.sh
@@ -130,9 +130,6 @@ end
   ".properties.on_demand_broker_dedicated_single_node_plan_rabbitmq_persistent_disk_type": {
     "value": $on_demand_broker_dedicated_single_node_plan_rabbitmq_persistent_disk_type
   },
-  ".properties.on_demand_broker_dedicated_single_node_plan_disk_limit_acknowledgement": {
-    "value": [ $on_demand_broker_dedicated_single_node_plan_disk_limit_acknowledgement ]
-  },
   ".properties.on_demand_broker_dedicated_cluster_plan_selector": {
     "value": $on_demand_broker_dedicated_cluster_plan_selector
   }
@@ -171,7 +168,7 @@ if $on_demand_broker_dedicated_cluster_plan_selector == "enabled" then
     "value": $on_demand_broker_dedicated_cluster_plan_selector_enabled_rabbitmq_vm_type
   },
   ".properties.on_demand_broker_dedicated_cluster_plan_selector.enabled.disk_limit_acknowledgement": {
-    "value": [ $on_demand_broker_dedicated_cluster_plan_selector_enabled_disk_limit_acknowledgement ]
+    "value": ( $on_demand_broker_dedicated_cluster_plan_selector_enabled_disk_limit_acknowledgement | split(",") )
   }
 }
 else .
