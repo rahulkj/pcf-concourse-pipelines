0b373668d85ebb7545fce0d2d9486daf82c30028
Rahul Jain <rahulkj@gmail.com>
2018-06-07 21:11:29 -0500

    Updated the ops mgr config task and also the pipelines

--- tasks/config-opsman/task.sh
+++ tasks/config-opsman/task.sh
@@ -10,11 +10,10 @@ chmod +x om-cli/om-linux
 
 CMD=./om-cli/om-linux
 
+printf 'waiting for OpsManager endpoint to be available'
 until $(curl --output /dev/null -k --silent --head --fail https://$OPS_MGR_HOST/setup); do
     printf '.'
     sleep 5
 done
-
-set -x
 
 $CMD -t https://$OPS_MGR_HOST -k configure-authentication -u $OPS_MGR_USR -p $OPS_MGR_PWD -dp $OM_DECRYPTION_PWD
