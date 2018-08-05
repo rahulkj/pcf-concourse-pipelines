5f5121d2f0bb86fdfcdf947b19e373df5105f8fe
Rahul Jain <rahulkj@gmail.com>
2018-08-05 13:41:00 -0500

    Testing apply changes

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -7,7 +7,29 @@ else
 fi
 
 chmod +x om-cli/om-linux
+OM_CMD=./om-cli/om-linux
 
-CMD=./om-cli/om-linux
+chmod +x ./jq/jq-linux64
+JQ_CMD=./jq/jq-linux64
 
-$CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD apply-changes  --ignore-warnings true
+echo "$APPLY_CHANGES_CONFIG" > apply_changes_config.yml
+apply_changes_config=$(ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' < apply_changes_config.yml)
+
+deploy_products_type=$(echo "$apply_changes_config" | jq '.deploy_products | type')
+
+if [[ "$deploy_products_type" == "array" ]]; then
+  products=$(echo "$apply_changes_config" | jq -r '.deploy_products[]')
+
+  staged_products=$($OM_CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -s -p /api/v0/staged/products)
+  for product in $(echo $products | sed "s/\n/ /g"); do
+    product_guid=$(echo "$STAGED_PRODUCTS" | jq -r --arg product_name $product '.[] | select(.type == $product_name) | .guid')
+    sed -i -e "s/$product/$product_guid/g" apply_changes_config.yml
+  done
+
+  apply_changes_config=$(ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' < apply_changes_config.yml)
+fi
+
+
+$OM_CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD curl -s -p /api/v0/installations -x POST -d "$JSON"
+
+$OM_CMD -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD apply-changes --ignore-warnings true
