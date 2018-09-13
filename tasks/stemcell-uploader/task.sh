cead390998a06390ce619852778e7e946ea4a84f
Lance French <lfrench@pivotal.io>
2018-09-13 13:18:02 -0700

    More fixes for my bash typoes.  Seriously, I'm done now.

--- tasks/stemcell-uploader/task.sh
+++ tasks/stemcell-uploader/task.sh
@@ -20,9 +20,9 @@ SC_VERSION=`cat ./pivnet-product/metadata.json | $JQ_CMD -r '.Dependencies[] | s
 if [[ ! -z "$SC_VERSION" ]]; then
   STEMCELL_NAME=bosh-stemcell-$SC_VERSION-$IAAS_TYPE-ubuntu-$STEMCELL_TYPE-go_agent.tgz
   if [ "$STEMCELL_TYPE"="xenial" ]; then
-    $PRODUCT_SLUG="stemcells-ubuntu-xenial"
+    PRODUCT_SLUG="stemcells-ubuntu-xenial"
   else
-    $PRODUCT_SLUG="stemcells"
+    PRODUCT_SLUG="stemcells"
   fi
 
   DIAGNOSTIC_REPORT=$($OM_CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -s -p /api/v0/diagnostic_report)
