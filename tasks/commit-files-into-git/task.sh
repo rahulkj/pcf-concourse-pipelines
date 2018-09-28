85e914219a02bac00a1bac937d9c7cdcdae9acd9
Rahul Jain <rahulkj@gmail.com>
2018-09-28 15:35:44 -0500

    Adding git file commits task - FIX

--- tasks/commit-files-into-git/task.sh
+++ tasks/commit-files-into-git/task.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $DEBUG == true ]]; then
-  set -ex
+  set -eux
 else
-  set -e
+  set -eu
 fi
 
-if [[ ! -d git_repo/$FILES_PATH ]]; then
+if [ ! -d "git_repo/$FILES_PATH" ]; then
   mkdir -p git_repo/$FILES_PATH
 fi
 
 cp -r src_dir/* git_repo/$FILES_PATH/
 
-cd git_repo
+pushd git_repo
+  git config --global user.email "${CI_EMAIL_ADDRESS}"
+  git config --global user.name "${CI_USERNAME}"
 
-git config --global user.email "${CI_EMAIL_ADDRESS}"
-git config --global user.name "${CI_USERNAME}"
-
-git add .
-git commit -m "$GIT_COMMIT_MESSAGE"
+  git add .
+  git commit -m "$GIT_COMMIT_MESSAGE"
+popd
