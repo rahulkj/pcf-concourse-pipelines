8979ce5009bf8a0042cf9a58f988322c288af9f4
Rahul Jain <rahulkj@gmail.com>
2018-10-16 20:59:26 -0500

    Fixing the bugs

--- tasks/stage-product/task.sh
+++ tasks/stage-product/task.sh
@@ -26,9 +26,8 @@ fi
 
 VERSION=`cat pivnet-product/metadata.json | jq '.Release.Version' | tr -d '"'`
 
-RELEASE_NAME=`$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k available-products | grep $PRODUCT_IDENTIFIER | grep $VERSION`
+RELEASE_NAME=`$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k available-products | grep $PRODUCT_NAME | grep $VERSION`
 
-PRODUCT_NAME=`echo $RELEASE_NAME | cut -d"|" -f2 | tr -d " "`
 PRODUCT_VERSION=`echo $RELEASE_NAME | cut -d"|" -f3 | tr -d " "`
 
 $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_NAME -v $PRODUCT_VERSION
