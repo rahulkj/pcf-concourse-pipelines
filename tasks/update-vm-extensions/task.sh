49c113266e14db506fb8b72ccecf2bf1037b4c80
Rahul Jain <rahulkj@gmail.com>
2018-10-20 14:35:21 -0500

    Adding logic to configure extensions 2

--- tasks/update-vm-extensions/task.sh
+++ tasks/update-vm-extensions/task.sh
@@ -9,6 +9,17 @@ fi
 chmod +x om-cli/om-linux
 CMD=./om-cli/om-linux
 
+chmod +x ./jq/jq-linux64
+JQ_CMD=./jq/jq-linux64
+
 JSON=$(cat config/$EXTENSION_FILE)
 
-$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -p /api/v0/staged/vm_extensions -d "$JSON" -x POST
+CURL_CMD=$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -s
+
+EXISTING_EXTENSION=$($CURL_CMD -p /api/v0/staged/vm_extensions)
+
+EXTENSION_FOUND=$(echo "$EXISTING_EXTENSION" | $JQ_CMD '.vm_extensions[] | select(.name | contains("router-extension"))')
+
+if [ -z "$EXTENSION_FOUND" ]; then
+  $CURL_CMD -p /api/v0/staged/vm_extensions -d "$JSON" -x POST
+fi
